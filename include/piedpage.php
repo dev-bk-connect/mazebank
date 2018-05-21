2815aad7099f8fc54166c3ae8365959ab6a9de49
dev-bk-connect <38334624+dev-bk-connect@users.noreply.github.com>
2018-05-21 22:58:06 +0200

    .

--- include/piedpage.php
+++ include/piedpage.php
@@ -1,5 +1,5 @@
 <nav class="navbar fixed-bottom navbar-expand-lg navbar-dark" style="background: linear-gradient(rgb(255, 0, 0), rgb(192, 0, 0));">
-  <a class="navbar-brand" href="http://mazebank">Maze Bank</a><span class="navbar-text navbar-center">BK-Connect &copy; <?php echo date("Y"); ?></span>
+  <a class="navbar-brand" href="">Maze Bank</a><span class="navbar-text navbar-center">BK-Connect &copy; <?php echo date("Y"); ?></span>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu2" aria-controls="navbarMenu2" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
